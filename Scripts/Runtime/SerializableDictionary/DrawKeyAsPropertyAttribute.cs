95d85c371c01dc7cd628c751f0de36b90b460865
김영찬 <youngchangoon521@gmail.com>
2020-05-07 17:36:16 +0900

    SerializableDictionary 추가 ( 외부 라이브러리 )

---
+++ Scripts/Runtime/SerializableDictionary/DrawKeyAsPropertyAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace MP_Patterns
+{
+    /// <summary>
+    /// Attribute used to force drawing a key as a property
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Field)]
+    public class DrawKeyAsPropertyAttribute : System.Attribute { }
+}
